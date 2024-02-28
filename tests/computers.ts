6aa831bebe1bd8d1e910090a84ced9b8990da7e5
mohamad khawam <khawammohamad99@gmail.com>
2024-02-28 18:14:33 -0500

    Password changing Test

--- tests/computers.ts
+++ tests/computers.ts
@@ -1,36 +1,31 @@
-import { ServerInfo, User } from "src/db/dbtypes";
+import { Server, ServerInfo, User } from "../src/db/dbtypes";
 import dotenv from "dotenv";
 dotenv.config();
 
 const defaultPassword = process.env.DEFAULT;
 
-const computers: ServerInfo[] = [
+const computers: Server[] = [
     {
-        "IP Address": "192.168.15.11",
+        ipaddress: "192.168.15.11",
         Name: "Ubuntu 24",
         "OS Type": "linux",
         domain: "",
         password_changes: 0,
-        users: ["ubuntu24"],
+        users: [
+            {
+                domain: "",
+                failedPasswords: [],
+                hostname: "Ubuntu 24",
+                ipaddress: "192.168.15.11",
+                password: defaultPassword || "Password123",
+                password_changes: 0,
+                ssh_key: false,
+                user_id: "ubuntu24",
+                username: "root",
+                oldPasswords: [],
+            },
+        ],
     },
 ];
-interface Users {
-    [key: string]: User;
-}
 
-const computerUsers: Users = {
-    ubuntu24: {
-        domain: "",
-        failedPasswords: [],
-        hostname: "Ubuntu 24",
-        ipaddress: "192.168.15.11",
-        password: defaultPassword || "Password123",
-        password_changes: 0,
-        ssh_key: false,
-        user_id: "ubuntu24",
-        username: "root",
-        oldPasswords: [],
-    },
-};
-
-export { computers, computerUsers };
+export { computers };
--- tests/ssh.spec.ts
+++ tests/ssh.spec.ts
@@ -1,10 +1,15 @@
-import { ejectSSHkey, makeConnection, removeSSHkey } from "../src/modules/util/ssh_utils";
-import { computers, computerUsers } from "./computers";
+import { changePasswordOf } from "src/modules/password/change_passwords";
+import { detect_hostname, detect_os, ejectSSHkey, makeConnection, removeSSHkey, testPassword } from "../src/modules/util/ssh_utils";
+import { computers } from "./computers";
 import assert from "assert";
+import dotenv from "dotenv";
+dotenv.config();
+
+const defaultPassword = process.env.DEFAULT;
 
 for (let computer of computers) {
-    describe(`SSH ${computer["OS Type"]} ${computer.Name} ${computer["IP Address"]}`, () => {
-        let user = computerUsers[computer.users[0]];
+    describe(`SSH ${computer["OS Type"]} ${computer.Name} ${computer.ipaddress}`, () => {
+        let user = computer.users[0];
         if (!user) {
             throw new Error("Unable to find User");
         }
@@ -12,6 +17,67 @@ for (let computer of computers) {
             let ssh = await makeConnection(user, 3000, 3);
             assert.ok(ssh, "Unable to connect to target server");
             if (ssh) await ssh.close();
+        });
+        describe("Getting Status", () => {
+            it("get hostname", async () => {
+                let ssh = await makeConnection(user, 3000, 3);
+                if (!ssh) {
+                    throw new Error("Unable to connect to target server");
+                }
+                let hostname = await detect_hostname(ssh);
+                assert.ok(hostname, "Unable to get hostname");
+                await ssh.close();
+            });
+
+            it("get os", async () => {
+                let ssh = await makeConnection(user, 3000, 3);
+                if (!ssh) {
+                    throw new Error("Unable to connect to target server");
+                }
+                let os = await detect_os(ssh);
+                assert.ok(os, "Unable to get hostname");
+                await ssh.close();
+            });
+        });
+
+        describe("Password Test", () => {
+            it("test current password", async () => {
+                let ssh = await makeConnection(user, 3000, 3);
+                if (!ssh) {
+                    throw new Error("Unable to connect to target server");
+                }
+                let password_active = await testPassword(ssh, user.password);
+
+                assert.ok(password_active, "User Password Check was false");
+                await ssh.close();
+            });
+            it("test incorrect password", async () => {
+                let ssh = await makeConnection(user, 3000, 3);
+                if (!ssh) {
+                    throw new Error("Unable to connect to target server");
+                }
+                let password_active = await testPassword(ssh, "RandomPassword");
+                assert.ok(!password_active, "User Password Check was true");
+                await ssh.close();
+            });
+            it("Can Change Password", async () => {
+                if (!defaultPassword) {
+                    throw new Error("Unable to change password no default password");
+                }
+                let currUser = user;
+                let passwordResult = await changePasswordOf(computer, currUser, defaultPassword + "123");
+                assert.ok(!(typeof passwordResult === "string"), passwordResult.toString());
+                if (typeof passwordResult === "string") {
+                    return;
+                }
+
+                currUser.password = defaultPassword + "123";
+                let passwordResultBack = await changePasswordOf(computer, currUser, defaultPassword);
+                assert.ok(!(typeof passwordResultBack === "string"), "Unable to change password back: " + passwordResult.toString());
+                if (typeof passwordResultBack === "string") {
+                    return;
+                }
+            });
         });
 
         describe("SSH Key", () => {
