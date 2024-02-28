ae825c6cace379b34cb52a93867be25010fc2f79
mohamad khawam <khawammohamad99@gmail.com>
2024-02-28 17:11:25 -0500

    Added SSH key testing

--- tests/ssh.spec.ts
+++ tests/ssh.spec.ts
@@ -1,4 +1,4 @@
-import { makeConnection } from "../src/modules/util/ssh_utils";
+import { ejectSSHkey, makeConnection, removeSSHkey } from "../src/modules/util/ssh_utils";
 import { computers, computerUsers } from "./computers";
 import assert from "assert";
 
@@ -12,6 +12,25 @@ for (let computer of computers) {
             let ssh = await makeConnection(user, 3000, 3);
             assert.ok(ssh, "Unable to connect to target server");
             if (ssh) await ssh.close();
+        });
+
+        it("Can deploy SSH key to Server", async () => {
+            let ssh = await makeConnection(user, 3000, 3);
+            if (!ssh) {
+                throw new Error("Unable to connect to target server");
+            }
+            let sshkey = await ejectSSHkey(ssh, computer["OS Type"]);
+            assert.ok(sshkey, "Unable to enject SSH key");
+            ssh.close();
+            it("Can remove SSH key to Server", async () => {
+                let ssh = await makeConnection(user, 3000, 3);
+                if (!ssh) {
+                    throw new Error("Unable to connect to target server");
+                }
+                let sshkey = await removeSSHkey(ssh, computer["OS Type"]);
+                assert.ok(sshkey, "Unable to remove SSH key");
+                ssh.close();
+            });
         });
     });
 }
