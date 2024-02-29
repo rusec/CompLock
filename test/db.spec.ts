8c77cdb19b37069141ec0422934acdff1704f95b
mohamad khawam <khawammohamad99@gmail.com>
2024-02-29 14:20:22 -0500

    added some tests to db

--- test/db.spec.ts
+++ test/db.spec.ts
@@ -4,6 +4,17 @@ describe("DataBase", () => {
     before(async () => {
         await db.writePassword("ThisIsTheTestPassword");
     });
+    describe("Add Computer", () => {
+        it("should add a user successfully", async () => {
+            const ip = "192.168.1.1";
+            const os = "linux";
+            const hostname = "myhost";
+            const domain = "example.com";
+            const result = await db.addTarget(hostname, ip, os, domain);
+
+            assert.ok(result);
+        });
+    });
     describe("Add User", () => {
         it("should add a user successfully", async () => {
             const ip = "192.168.1.1";
@@ -14,7 +25,18 @@ describe("DataBase", () => {
 
             const result = await db.addUser(ip, username, password, hostname, domain);
 
-            assert.ok(result);
+            assert.ok(result, "unable to add user to pc");
+        });
+        it("shouldn't add user to computer that doesn't exist", async () => {
+            const ip = "192.168.1.2";
+            const username = "testuser";
+            const password = "secretpassword";
+            const hostname = "myhost";
+            const domain = "example.com";
+
+            const result = await db.addUser(ip, username, password, hostname, domain);
+
+            assert.ok(!result, "added user to computer that doesnt exist");
         });
     });
 });
